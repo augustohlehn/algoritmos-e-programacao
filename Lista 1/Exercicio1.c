82fa4b919020346e8c5ac46b18b7ab41c363c863
augustohlehn <augustolehnen18@gmail.com>
2024-09-03 20:47:50 -0300

    Create Exercicio1.c

---
+++ Lista 1/Exercicio1.c
@@ -0,0 +1,16 @@
+//Em linguagem C:
+#include <stdio.h>
+int main()
+{
+    printf("Olá mundo!");//Comando de saída.
+
+    return 0;
+}
+
+
+//Em linguagem Python:
+print("Olá mundo!");//Comando de saída.
+
+
+//Em linguagem PHP:
+echo "Olá mundo!";//Comando de saída.
